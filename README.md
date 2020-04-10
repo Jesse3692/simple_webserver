5ea0fd755d80cfa2292f1719fbcc79489b5a5928
Jesse <ab9421@126.com>
2020-04-11 00:32:21 +0800

    将响应内容修改为符合HTTP响应的格式

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# simple_webserver
+# 实现一个Web服务器
+
+## 实现最简单的Web服务器
+
+这个实现比较简单，就是监听8080端口，如果有请求在终端进行打印，并返回"Hello, World!"的响应。
+
+
+[![G7aHiQ.png](https://s1.ax1x.com/2020/04/11/G7aHiQ.png)](https://imgchr.com/i/G7aHiQ)
+
+[![G7UJ1I.png](https://s1.ax1x.com/2020/04/11/G7UJ1I.png)](https://imgchr.com/i/G7UJ1I)
+
+[![G7Us9s.png](https://s1.ax1x.com/2020/04/11/G7Us9s.png)](https://imgchr.com/i/G7Us9s)
--- s1/server.py
+++ s1/server.py
@@ -1,3 +1,15 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    : server.py
+@Time    : 2020/04/10 23:59:07
+@Author  : Jesse Chang
+@Contact : jessechang2358@gmail.com
+@Version : 0.1
+@License : Apache License Version 2.0, January 2004
+@Desc    : None
+'''
+
+
 import socket
 
 listener = socket.socket()
@@ -7,15 +19,11 @@ listener.listen(1)
 print('Serving HTTP on 0.0.0.0 port 8080 ...')
 
 while True:
-    client_connection, client_address = \
+    conn, addr = \
         listener.accept()
-    print(f'Server received connection from {client_address}')
-    request = client_connection.recv(1024)
+    print(f'Server received connection from {addr}')
+    request = conn.recv(1024)
     print(f'request we received: {request}')
-    response = b"""
-    HTTP/1.1 200 OK
-    
-    Hello, World!
-    """
-    client_connection.sendall(response)
-    client_connection.close()
+    response = b'HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n'
+    conn.sendall(response)
+    conn.close()
